fdeb376bed90f8637b773c16f41a6a978243bc7b
Pavlo Sirous <pashka4281@gmail.com>
2016-04-27 11:24:39 -0400

    init

---
+++ bot.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+#
+#
+require "./lib/greenbot.rb"
+require "timeout"
+
+tell "Thank you for texting us. This KISST bot is currently under construction. If you are looking for the owner of this number, I can take a message."
+if confirm("Would you like someone to contact you?")
+  contact_me = true
+  contact_me.remember("contact_me")
+  name = ask("When we call, who should we ask for?")
+  name.remember("who_to_ask_for")
+  if confirm("Is there another number we should try?")
+    better_number = ask("Please enter that number with an area code")
+    better_number.remember("better_number")
+  end
+end
+if confirm("Would you like to leave a message?")
+  issue = note("How can we help you? Please use as many messages as you need.")
+  tell "Thank you. We will forward the message."
+end
+tell "Goodbye."
